af6b12753565ecd79093615bcf2a05b579c40b37
Sean Hannaford <shannaford17@gmail.com>
2025-03-25 20:17:50 -0400

    Added next and back buttons to scheduler header, needs work

--- server/routes/router.js
+++ server/routes/router.js
@@ -14,4 +14,17 @@ router.get('/weeklySchedule', (req, res) => {
         })    
 })
 
+router.get('/remainingSchedule', (req, res) => {
+    
+    fetch('https://api-web.nhle.com/v1/club-schedule-season/NJD/now')
+        .then((res) => {
+            if (res) {
+                return res.json()
+            }
+        })
+        .then((data) => {
+            res.send(data)
+        })    
+})
+
 module.exports = router
--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Card, CardContent, Typography, Box } from '@mui/material';
+import { Card, CardContent, Typography, Box, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid2';
+import * as React from 'react';
 
 function ScheduleComponent(props) {
+
+  const [page,setPage] = useState(0)
 
   if (!props.games){
     return
@@ -12,31 +15,55 @@ function ScheduleComponent(props) {
   function scheduleCard(game) {
 
     return(
-      <Card sx={{width:300}}>
-        <CardContent>        
-          <div>
-            <Typography gutterBottom sx={{fontSize: 18}}>
-              {game.gameDate}
-            </Typography>
-          </div>
-          <Box sx={{display: "flex", alignItems: 'center', pl: 1, pb: 1 }}>        
-              <Typography gutterBottom sx={{fontSize: 18, width: 100}}>
-                {game.awayTeam.abbrev}
-              </Typography>
-              <Typography gutterBottom sx={{fontSize: 18, width: 100}}>
-                {game.homeTeam.abbrev}
-              </Typography>
-          </Box>
-        </CardContent>     
-      </Card>
+      <React.Fragment>
+        <Grid item size={100}>
+          <Card className='gamesCard' sx={{width:'100%'}}>
+            <CardContent>        
+              <div>
+                <Typography gutterBottom sx={{fontSize: 18}}>
+                  {game.gameDate}
+                </Typography>
+              </div>
+              <Box sx={{display: "flex", alignItems: 'center'}}>        
+                  <Typography gutterBottom sx={{fontSize: 18, }}>
+                    {game.awayTeam.abbrev}
+                  </Typography>
+                  <Typography gutterBottom sx={{fontSize: 18, }}>
+                    {game.homeTeam.abbrev}
+                  </Typography>
+              </Box>
+            </CardContent>     
+          </Card>
+        </Grid>
+      </React.Fragment>      
     )
 
   }
   
-  return( 
-    <div style={{display: "flex"}}>
-      {props.games.map(game => scheduleCard(game))}
-    </div>    
+  return(     
+    <React.Fragment>      
+      <Grid item size={1}>
+        <Button className="scheduleBackBtn"
+          variant='contained'
+          sx={{height: '100%'}}
+          onClick={() => setPage(page-4)}>        
+        </Button>
+      </Grid>    
+      <Grid container item size={10}>
+        <div className='gamesContainer' style={{display: "flex", width: '100%'}}>
+          {props.games.slice(page, page+5).map(game => scheduleCard(game))}        
+        </div>      
+      </Grid>         
+      <Grid item size={1}>
+        <Button className="scheduleNextBtn"
+          variant='contained'
+          
+          sx={{height: '100%'}}
+          onClick={() => setPage(page+4)}
+        >        
+        </Button>
+      </Grid>                   
+    </React.Fragment>
   )
 }
 
@@ -45,7 +72,7 @@ function App() {
   const [res, setRes] = useState([])
 
   useEffect(() => {
-    fetch("http://localhost:8080/weeklySchedule", {
+    fetch("http://localhost:8080/remainingSchedule", {
       method: "GET",      
     }).then((response) => response.json())
     .then((json) => {
@@ -54,13 +81,15 @@ function App() {
   }, [])
 
   return (
-    <div className="App">      
-      <div className="mainContainer">
-        <ScheduleComponent 
-          games={res.games}
-        />
-      </div>
-    </div>
+    <Box className="App">      
+      <Grid container className="mainContainer">
+        <Grid container item size={12}>
+          <ScheduleComponent
+            games={res.games}
+          />              
+        </Grid>        
+      </Grid>        
+    </Box>      
   );
 }
 
